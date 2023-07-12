45981c6e5ffc3a9922c9852fd7b5391fad158c24
Matheus Fontes <matheusfontes1921@gmail.com>
2023-07-12 11:33:19 -0300

    starting project

---
+++ supera-challenge-frontend/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version:'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js','.jsx','.ts','.tsx'],
+      },
+    },
+  },
+  env: { browser: true, amd: true, node: true, },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+}
---
+++ supera-challenge-frontend/.pretierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, /*adiciona ponto vírgula*/
+    trailingComma: 'all', /**/
+    singleQuote: false, /*aspas simples ao inves de aspas duplas*/
+    printWidth: 100, /*onde o código quebra*/
+    tabWidth: 2, /*tamanho do tab*/
+};
